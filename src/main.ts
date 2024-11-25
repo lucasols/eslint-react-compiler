c3a8058ba7fd4378b805d42fca09705a6ad0efd5
lucaslos <lucas@sugestly.com>
2024-11-24 21:06:50 -0300

    Rename extendedLintPlugin to reactCompilerPlugin in src/main.ts for better clarity and alignment with project structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { TSESLint } from '@typescript-eslint/utils'
 import { rules } from './rules/rules'
 
-export const extendedLintPlugin: TSESLint.FlatConfig.Plugin = {
+export const reactCompilerPlugin: TSESLint.FlatConfig.Plugin = {
   rules,
 }
